dce068535fdce95748c1539f36c0111ccc94bf5f
FahimChoudhury007 <fahim.choudhury@berkeley.edu>
2024-04-19 11:37:33 -0700

    Added Project

---
+++ src/drone_proj3/drone_estimator.py
@@ -0,0 +1,368 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from dynamics import QuadDyn
+import sympy as sym
+from tqdm import tqdm
+import time
+
+
+plt.rcParams['font.family'] = ['Arial']
+plt.rcParams['font.size'] = 14
+
+
+class Estimator:
+    """A base class to represent an estimator.
+
+    This module contains the basic elements of an estimator, on which the
+    subsequent DeadReckoning, Kalman Filter, and Extended Kalman Filter classes
+    will be based on. A plotting function is provided to visualize the
+    estimation results in real time.
+
+    Attributes:
+    ----------
+        u : list
+            A list of system inputs, where, for the ith data point u[i],
+            u[i][1] is the thrust of the quadrotor
+            u[i][2] is right wheel rotational speed (rad/s).
+        x : list
+            A list of system states, where, for the ith data point x[i],
+            x[i][0] is translational position in x (m),
+            x[i][1] is translational position in z (m),
+            x[i][2] is the bearing (rad) of the quadrotor
+            x[i][3] is translational velocity in x (m/s),
+            x[i][4] is translational velocity in z (m/s),
+            x[i][5] is angular velocity (rad/s),
+        y : list
+            A list of system outputs, where, for the ith data point y[i],
+            y[i][1] is distance to the landmark (m)
+            y[i][2] is relative bearing (rad) w.r.t. the landmark
+        x_hat : list
+            A list of estimated system states. It should follow the same format
+            as x.
+        dt : float
+            Update frequency of the estimator.
+        fig : Figure
+            matplotlib Figure for real-time plotting.
+        axd : dict
+            A dictionary of matplotlib Axis for real-time plotting.
+        ln* : Line
+            matplotlib Line object for ground truth states.
+        ln_*_hat : Line
+            matplotlib Line object for estimated states.
+        canvas_title : strIndexError: index 2 is out of bounds for axis 0 with size 2
+            Title of the real-time plot, which is chosen to be estimator type.
+
+    Notes
+    ----------
+        The landmark is positioned at (0, 5, 5).
+    """
+    # noinspection PyTypeChecker
+    def __init__(self, is_noisy=False):
+        self.u = []
+        self.x = []
+        self.y = []
+        self.x_hat = []  # Your estimates go here!
+        self.t = []
+        self.fig, self.axd = plt.subplot_mosaic(
+            [['xz', 'phi'],
+             ['xz', 'x'],
+             ['xz', 'z']], figsize=(20.0, 10.0))
+        self.ln_xz, = self.axd['xz'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_xz_hat, = self.axd['xz'].plot([], 'o-c', label='Estimated')
+        self.ln_phi, = self.axd['phi'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_phi_hat, = self.axd['phi'].plot([], 'o-c', label='Estimated')
+        self.ln_x, = self.axd['x'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_x_hat, = self.axd['x'].plot([], 'o-c', label='Estimated')
+        self.ln_z, = self.axd['z'].plot([], 'o-g', linewidth=2, label='True')
+        self.ln_z_hat, = self.axd['z'].plot([], 'o-c', label='Estimated')
+        self.canvas_title = 'N/A'
+
+        # Defined in dynamics.py for the dynamics model
+        # m is the mass and J is the moment of inertia of the quadrotor 
+        self.gr = 9.81 
+        self.m = 0.92
+        self.J = 0.0023
+        # These are the X, Y, Z coordinates of the landmark
+        self.landmark = (0, 5, 5)
+
+        # This is a (N,12) where it's time, x, u, then y_obs 
+        if is_noisy:
+            with open('noisy_data.npy', 'rb') as f:
+                self.data = np.load(f)
+        else:
+            with open('data.npy', 'rb') as f:
+                self.data = np.load(f)
+
+        self.dt = self.data[-1][0]/self.data.shape[0]
+
+
+    def run(self):
+        
+        comp_times = np.array([])
+        squared_differences = np.array([])
+        for i, data in enumerate(self.data):
+            self.t.append(np.array(data[0]))
+            self.x.append(np.array(data[1:7]))
+            self.u.append(np.array(data[7:9]))
+            self.y.append(np.array(data[9:12]))
+            if i == 0:
+                self.x_hat.append(self.x[-1])
+            else:
+                start_time = time.time()
+                self.update(i)
+                end_time = time.time()
+                comp_times = np.append(comp_times, end_time - start_time)
+                squared_differences = np.append(squared_differences, (self.x[-1] - self.x_hat[-1]) ** 2)
+        
+        avg_time_step = np.mean(comp_times)
+        avg_sq_dif = np.mean(squared_differences)
+
+        print(f"Average Per-Step Computational Running Time: {avg_time_step}")
+        print(f"Average Square Error: {avg_sq_dif}")
+        return self.x_hat
+
+    def update(self, _):
+        raise NotImplementedError
+
+    def plot_init(self):
+        self.axd['xz'].set_title(self.canvas_title)
+        self.axd['xz'].set_xlabel('x (m)')
+        self.axd['xz'].set_ylabel('z (m)')
+        self.axd['xz'].set_aspect('equal', adjustable='box')
+        self.axd['xz'].legend()
+        self.axd['phi'].set_ylabel('phi (rad)')
+        self.axd['phi'].set_xlabel('t (s)')
+        self.axd['phi'].legend()
+        self.axd['x'].set_ylabel('x (m)')
+        self.axd['x'].set_xlabel('t (s)')
+        self.axd['x'].legend()
+        self.axd['z'].set_ylabel('z (m)')
+        self.axd['z'].set_xlabel('t (s)')
+        self.axd['z'].legend()
+        plt.tight_layout()
+
+    def plot_update(self, _):
+        self.plot_xzline(self.ln_xz, self.x)
+        self.plot_xzline(self.ln_xz_hat, self.x_hat)
+        self.plot_philine(self.ln_phi, self.x)
+        self.plot_philine(self.ln_phi_hat, self.x_hat)
+        self.plot_xline(self.ln_x, self.x)
+        self.plot_xline(self.ln_x_hat, self.x_hat)
+        self.plot_zline(self.ln_z, self.x)
+        self.plot_zline(self.ln_z_hat, self.x_hat)
+
+    def plot_xzline(self, ln, data):
+        if len(data):
+            x = [d[0] for d in data]
+            z = [d[1] for d in data]
+            ln.set_data(x, z)
+            self.resize_lim(self.axd['xz'], x, z)
+
+    def plot_philine(self, ln, data):
+        if len(data):
+            t = self.t
+            phi = [d[2] for d in data]
+            ln.set_data(t, phi)
+            self.resize_lim(self.axd['phi'], t, phi)
+
+    def plot_xline(self, ln, data):
+        if len(data):
+            t = self.t
+            x = [d[0] for d in data]
+            ln.set_data(t, x)
+            self.resize_lim(self.axd['x'], t, x)
+
+    def plot_zline(self, ln, data):
+        if len(data):
+            t = self.t
+            z = [d[1] for d in data]
+            ln.set_data(t, z)
+            self.resize_lim(self.axd['z'], t, z)
+
+    # noinspection PyMethodMayBeStatic
+    def resize_lim(self, ax, x, y):
+        xlim = ax.get_xlim()
+        ax.set_xlim([min(min(x) * 1.05, xlim[0]), max(max(x) * 1.05, xlim[1])])
+        ylim = ax.get_ylim()
+        ax.set_ylim([min(min(y) * 1.05, ylim[0]), max(max(y) * 1.05, ylim[1])])
+
+class OracleObserver(Estimator):
+    """Oracle observer which has access to the true state.
+
+    This class is intended as a bare minimum example for you to understand how
+    to work with the code.
+
+    Example
+    ----------
+    To run the oracle observer:
+        $ python drone_estimator_node.py --estimator oracle_observer
+    """
+    def __init__(self, is_noisy=False):
+        super().__init__(is_noisy)
+        self.canvas_title = 'Oracle Observer'
+
+    def update(self, _):
+        self.x_hat.append(self.x[-1])
+
+
+class DeadReckoning(Estimator):
+    """Dead reckoning estimator.
+
+    Your task is to implement the update method of this class using only the
+    u attribute stateDimn=8, inputDimn=2,  and x0. You will need to build a model of the unicycle model
+    with the parameters provided to you in the lab doc. After building the
+    model, use the provided inputs to estimate system state over time.
+
+    The method should closely predict the state evolution if the system is
+    free of noise. You may use this knowledge to verify your implementation.
+
+    Example
+    ----------
+    To run dead reckoning:
+        $ python drone_estimator_node.py --estimator dead_reckoning
+    """
+    def __init__(self, is_noisy=False):
+        super().__init__(is_noisy)
+        self.canvas_title = 'Dead Reckoning'
+
+    def update(self, _):
+        if len(self.x_hat) > 0:
+            # TODO: Your implementation goes here!
+            # You may ONLY use self.u and self.x[0] for estimation
+            # self.x_hat = [self.x[0]]               ##self.x and self.u are list of lists 
+            self.x_hat = []
+            x = self.x[0]
+            x_array = np.array([[x[0]], [0], [x[1]], [x[2]], [x[3]], [0], [x[4]], [x[5]]])
+            dynamics = QuadDyn(x_array, m=self.m, Ixx=self.J)
+            t = 0
+            for u in self.u:
+                u_array = np.array([[u[0]], [u[1]]])
+                x_next = dynamics.integrate(u_array, t, self.dt)
+                x_next = x_next[:,0].tolist()
+                x_next = [x_next[0], x_next[2], x_next[3], x_next[4], x_next[6], x_next[7]]
+                self.x_hat.append(x_next)
+                t += self.dt
+
+# noinspection PyPep8Naming
+class ExtendedKalmanFilter(Estimator):
+    """Extended Kalman filter estimator.
+
+    Your task is to implement the update method of this class using the u
+    attribute, y attribute, and x0. You will need to build a model of the
+    unicycle model and linearize it at every operating point. After building the
+    model, use the provided inputs and outputs to estimate system state over
+    time via the recursive extended Kalman filter update rule.
+
+    Hint: You may want to reuse your code from DeadReckoning class and
+    KalmanFilter class.
+
+    Attributes:
+    ----------
+        landmark : tuple
+            A tuple of the coordinates of the landmark.
+            landmark[0] is the x coordinate.
+            landmark[1] is the y coordinate.
+            landmark[2] is the z coordinate.
+
+    Example
+    ----------
+    To run the extended Kalman filter:
+        $ python drone_estimator_node.py --estimator extended_kalman_filter
+    """
+    def __init__(self, is_noisy=False):
+        super().__init__(is_noisy)
+        self.canvas_title = 'Extended Kalman Filter'
+        # TODO: Your implementation goes here!
+        # You may define the Q, R, and P matrices below.
+        self.A = None
+        self.B = None
+        self.C = None
+        self.Q = np.eye(6) *10             ### Choose these matrices
+        self.P = np.eye(6) *1.0
+        self.R = np.eye(2) *0.01
+      
+
+    # noinspection DuplicatedCode
+    def update(self, i):
+
+        if len(self.x_hat) > 0: # and self.x_hat[-1][0] < self.x[-1][0]:
+
+            # TODO: Your implementation goes here!
+            # You may use self.u, self.y, and self.x[0] for estimation
+            x = np.asarray(self.x_hat[-1])
+            y = np.array(self.y[-1])
+            u = np.array(self.u[-1])
+            dt = self.dt
+
+            Q = self.Q
+            R = self.R
+            P = self.P
+
+            x_next = self.g(x,u)
+
+            ## Dynamics Linearization
+            A = self.approx_A(x,u)
+
+            ## Covariance Extrapolation
+            P = A@P@A.T + Q
+
+            ## Measurement Linearization
+            C = self.approx_C(x_next)
+
+            # Kalman Gain
+            K = P @ C.T@np.linalg.inv(C@P@C.T + R)
+
+            x = x_next + K@(y-self.h(x_next,y))
+
+            ## Covariance Update ##
+            P = (np.eye(6) - K@C)@P
+
+            self.x_hat.append(tuple(x))
+           
+
+    def g(self, x, u):
+        x = np.squeeze(x)
+        u = np.squeeze(u)
+        x = np.array(x)
+       
+
+        x_dot = np.array([0.0,0.0,0.0,0.0,0.0,0.0])
+        x_dot[0] = x[3]
+        x_dot[1] = x[4]
+        x_dot[2] = x[5]
+        x_dot[3] = -u[0]*np.sin(x[2])/self.m
+        x_dot[4] = -self.gr + u[0]*np.cos(x[2])/self.m
+        x_dot[5] = u[1]/self.J
+
+        return x + x_dot * self.dt
+
+    def h(self, x, y_obs):
+        x = np.squeeze(x)
+        x = np.array(x)
+        l = self.landmark
+        h = np.array([np.sqrt((l[0] -x[0])**2 + l[1]**2 + (l[2] - x[1])**2),x[2]]).T
+
+        return h
+
+    def approx_A(self, x, u):
+        x = np.squeeze(x)
+        u = np.squeeze(u)
+        phi = x[2]
+
+        A = np.array([[1.0,0.0,0.0,self.dt,0.0,0.0],
+                      [0.0,1.0,0.0,0.0,self.dt,0.0],
+                      [0.0,0.0,1.0,0.0,0.0,self.dt],
+                      [0.0,0.0,-u[0]*np.cos(phi)/self.m*self.dt,1.0,0.0,0.0],
+                      [0.0,0.0,-u[0]*np.sin(phi)/self.m*self.dt,0.0,1.0,0.0],
+                      [0.0,0.0,0.0,0.0,0.0,1.0]])
+        
+        return A
+    
+    def approx_C(self, x):
+        x= np.squeeze(x)
+        l = self.landmark
+        C = np.array([[-(l[0] - x[0])/np.sqrt((l[0] - x[0])**2 + l[1]**2 + (l[2] - x[1])**2 ),-(l[2] - x[1])/np.sqrt((l[0] - x[0])**2 + self.landmark[1]**2 + (l[2] - x[1])**2 ),0.0,0.0,0.0,0.0],
+                      [0.0,0.0,1.0,0.0,0.0,0.0]])
+        
+        return C
+
